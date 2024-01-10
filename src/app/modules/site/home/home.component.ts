5dc5697dd174d881ef1b73e29e87258b4dab560c
sidoine-projects <sidoinea96@gmail.com>
2024-01-10 18:38:58 +0100

    ok

---
+++ src/app/modules/site/home/home.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: [
+    './home.component.css',
+
+    // '../../../../assets/style.css',
+    // '../../../../assets/css/animate.min.css',
+    // '../../../../assets/css/bootstrap.min.css',
+    // '../../../../assets/css/datepicker.css',
+    // '../../../../assets/css/font-awesome.min.css',
+    // '../../../../assets/css/icofont.css',
+    // '../../../../assets/css/magnific-popup.css',
+    // '../../../../assets/css/nice-select.css',
+    // '../../../../assets/css/normalize.css',
+    // '../../../../assets/css/owl-carousel.css',
+    // '../../../../assets/css/responsive.css',
+    // '../../../../assets/css/slicknav.min.css'
+]
+})
+export class HomeComponent {
+
+}
